0ad9c878f1501c9afa3b08aa7c0b18df533b80a9
victormoraisc <victorcarvalho2019@gmail.com>
2023-12-02 10:40:07 -0300

    02/12/2023

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
 function headerScroll(){
     var headerNav = document.querySelector('.header');
+    var sandwitch = document.querySelectorAll('.lines');
     var logoLight = document.querySelector('.logo-light');
     var logoDark = document.querySelector('.logo-dark');
     if (window.scrollY > 0) {
       headerNav.classList.add('toggled');
       logoDark.style.display = 'block';
       logoLight.style.display = 'none';
-      
+      sandwitch.forEach(sandwitch => {
+        sandwitch.classList.add('toggled');
+      })
     } else {
       headerNav.classList.remove('toggled');
       logoDark.style.display = 'none';
       logoLight.style.display = 'block';
+      sandwitch.forEach(sandwitch => {
+        sandwitch.classList.remove('toggled');
+      })
     }
   }
 
